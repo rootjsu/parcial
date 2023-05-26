0ee6a26df25902ff2430b40534fe4583b4a13845
rootjsu <126358742+rootjsu@users.noreply.github.com>
2023-05-26 08:08:52 -0500

    Update ImagenInicio.jsx

--- src/Componente/ImagenInicio.jsx
+++ src/Componente/ImagenInicio.jsx
@@ -10,13 +10,7 @@ export const ImagenInicio = () => {
           src="https://cdn.pixabay.com/photo/2017/05/08/19/35/cyber-security-2296269_1280.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h1>SecureNet</h1>
-          <p>
-            En SecureNet, estamos comprometidos con tu privacidad y seguridad en
-            línea. 
-          </p>
-        </Carousel.Caption>
+       
       </Carousel>
     </div>
   );
